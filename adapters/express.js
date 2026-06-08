4d282836cfad0fc1c09ad4ceae46b5354bda3bed
croty-mm <clement.roty@mumnet.com>
2026-06-08 17:17:34 +0200

    Test vert

---
+++ adapters/express.js
@@ -0,0 +1,6 @@
+function guard(strategy)                    { }
+function loginRoute(strategy)               { }
+function callbackRoute(strategy, backchannel) { }
+function backchannelRoute(backchannel)      { }
+
+module.exports = { guard, loginRoute, callbackRoute, backchannelRoute };
