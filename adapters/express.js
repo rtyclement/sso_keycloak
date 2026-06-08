df9591b57a37ab215f4e8e08a6fca48c7c8d1ea4
croty-mm <clement.roty@mumnet.com>
2026-06-08 17:27:48 +0200

    Test true

--- adapters/express.js
+++ adapters/express.js
@@ -18,7 +18,19 @@ function loginRoute(strategy) {
         if (decision.type === 'redirect') return res.redirect(decision.url);
     };
 }
-function callbackRoute(strategy, backchannel) { }
+function callbackRoute(strategy, backchannel) {
+    return async (req, res) => {
+        const ctx = {
+            session: req.session,
+            url:     new URL(req.originalUrl, `${req.protocol}://${req.headers.host ?? 'localhost'}`),
+        };
+        const decision = await strategy.handleCallback(ctx);
+        if (decision.type === 'session') {
+            backchannel.trackSession(req.session.user?.sid, req.sessionID);
+            return res.redirect(decision.redirectTo);
+        }
+    };
+}
 function backchannelRoute(backchannel)      { }
 
 module.exports = { guard, loginRoute, callbackRoute, backchannelRoute };
