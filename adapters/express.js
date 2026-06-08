c85a9685e02ce62d5957f65dbf1db4cfe238a4e5
croty-mm <clement.roty@mumnet.com>
2026-06-08 17:21:09 +0200

    Test Vert

--- adapters/express.js
+++ adapters/express.js
@@ -1,4 +1,15 @@
-function guard(strategy)                    { }
+function guard(strategy) {
+    return async (req, res, next) => {
+        const decision = await strategy.authenticate({
+            session: req.session,
+            headers: req.headers,
+        });
+        if (decision.type === 'allow') {
+            req.principal = decision.principal;
+            return next();
+        }
+    };
+}
 function loginRoute(strategy)               { }
 function callbackRoute(strategy, backchannel) { }
 function backchannelRoute(backchannel)      { }
