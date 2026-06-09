51ab2ad697d9dfdc470d7fcde4c29514c7b736c3
croty-mm <clement.roty@mumnet.com>
2026-06-09 09:15:51 +0200

    test vert

--- adapters/Adapter.js
+++ adapters/Adapter.js
@@ -5,7 +5,18 @@ class Adapter {
         this._driver = driver;
     }
 
-    guard(strategy)                    { }
+    guard(strategy) {
+        return async (req, reply, next) => {
+            const decision = await strategy.authenticate({
+                session: this._driver.getSession(req),
+                headers: this._driver.getHeaders(req),
+            });
+            if (decision.type === 'allow') {
+                this._driver.setPrincipal(req, decision.principal);
+                return this._driver.continue(next);
+            }
+        };
+    }
     loginRoute(strategy)               { }
     callbackRoute(strategy, backchannel){ }
     backchannelRoute(backchannel)      { }
