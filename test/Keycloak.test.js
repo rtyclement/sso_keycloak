ad8a4012c30ea415fb6878ef7be7bf8af72f8a7f
croty-mm <clement.roty@mumnet.com>
2026-06-11 11:59:53 +0200

    protect accepte le parametre null et créer le pattern cohérent avec

--- test/Keycloak.test.js
+++ test/Keycloak.test.js
@@ -124,3 +124,22 @@ test('protect accepte un tableau de routes', () => {
     assert.equal(kc.getRules().length, 1);
     assert.equal(kc.getRule(0).patterns.length, 2);
 });
+
+test('protect empile les règles sans écraser', () => {
+    const kc  = new Keycloak({ framework: 'express' });
+    kc.protect({}, '/swagger', 'session');
+    kc.protect({}, null, 'bearer');
+
+    assert.equal(kc._rules.length, 2);
+    assert.equal(kc._rules[0].mode, 'session');
+    assert.equal(kc._rules[1].mode, 'bearer');
+});
+
+test('protect accepte routes null', () => {
+    const kc  = new Keycloak({ framework: 'express' });
+    kc.protect({}, null, 'bearer');
+    console.log(JSON.stringify(kc.getRules()));
+    assert.equal(kc._rules[0].patterns.length, 1);
+    assert.ok(kc._rules[0].patterns[0].test('/nimporte/quoi'));
+    assert.ok(!kc._rules[0].patterns[0].test('gnfngfngfn'));
+});
