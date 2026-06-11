d1188931b7f49362421f3e237fe9d85582d303b1
croty-mm <clement.roty@mumnet.com>
2026-06-11 10:18:01 +0200

    Test au vert

--- src/Keycloak.js
+++ src/Keycloak.js
@@ -1,3 +1,9 @@
-class Keycloak {}
+class Keycloak {
+    constructor({framework} = {}){
+        if(!framework || !['express','fastify'].includes(framework)){
+            throw new Error("[Keycloak] Erreur dans le choix du framework. Framework supporté : 'Express' et 'Fastify'");
+        }
+    }
+}
 
 module.exports = Keycloak;
