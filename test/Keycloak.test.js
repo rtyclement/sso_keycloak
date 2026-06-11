dceac6ace9af3415ee48c0684d80364e537c48e1
croty-mm <clement.roty@mumnet.com>
2026-06-11 10:12:11 +0200

    Test de la véification des frameworks choisis dans le constructeur Keycloak

--- test/Keycloak.test.js
+++ test/Keycloak.test.js
@@ -5,3 +5,19 @@ const Keycloak = require('../src/Keycloak');
 test('Keycloak est une classe instanciable', () => {
     assert.equal(typeof Keycloak, 'function');
 });
+
+test('Keycloak échoue si framework est absent', () => {
+    assert.throws(() => new Keycloak({}), /framework/i);
+});
+
+test('Keycloak échoue si framework est invalide', () => {
+    assert.throws(() => new Keycloak({ framework: 'django' }), /framework/i);
+});
+
+test('Keycloak accepte express', () => {
+    assert.doesNotThrow(() => new Keycloak({ framework: 'express' }));
+});
+
+test('Keycloak accepte fastify', () => {
+    assert.doesNotThrow(() => new Keycloak({ framework: 'fastify' }));
+});
