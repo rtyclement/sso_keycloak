25e4c80bf3451fccc12aece7afa4581252cbb7ca
croty-mm <clement.roty@mumnet.com>
2026-06-12 09:44:00 +0200

    Test smoke d'export de Keycloak et des drivers via l'index

--- test/index.test.js
+++ test/index.test.js
@@ -1,3 +1,9 @@
 const { test } = require('node:test');
 const assert   = require('node:assert/strict');
-const { } = require('../src/index');
+
+test('index exporte Keycloak et DRIVERS', () => {
+    const { Keycloak, DRIVERS } = require('../src/index');
+    assert.equal(typeof Keycloak, 'function');
+    assert.ok(DRIVERS.EXPRESS);
+    assert.ok(DRIVERS.FASTIFY);
+});
